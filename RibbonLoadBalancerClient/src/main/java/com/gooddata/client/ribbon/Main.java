49adcbeb46f4ea45f1f2d8a4cbd013420f0b6629
Lukas Krecan <lukas.krecan@gooddata.com>
2015-09-03 16:23:39 +0200

    Config added

--- RibbonLoadBalancerClient/src/main/java/com/gooddata/client/ribbon/Main.java
+++ RibbonLoadBalancerClient/src/main/java/com/gooddata/client/ribbon/Main.java
@@ -3,8 +3,7 @@
  */
 package com.gooddata.client.ribbon;
 
-import static java.util.Arrays.asList;
-
+import java.io.IOException;
 import java.net.URI;
 
 import org.slf4j.Logger;
@@ -15,20 +14,26 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.netflix.client.ClientFactory;
+import com.netflix.config.ConfigurationManager;
+import com.netflix.loadbalancer.BaseLoadBalancer;
 import com.netflix.loadbalancer.ILoadBalancer;
 import com.netflix.loadbalancer.IPing;
-import com.netflix.loadbalancer.LoadBalancerBuilder;
 import com.netflix.loadbalancer.Server;
 
 public class Main {
     private static Logger log = LoggerFactory.getLogger(Main.class);
     private final RestTemplate restTemplate;
 
-    public Main() {
+    public Main() throws IOException {
         SimpleClientHttpRequestFactory wrappedFactory = new SimpleClientHttpRequestFactory();
-        ILoadBalancer loadBalancer  = LoadBalancerBuilder.newBuilder()
-                .withPing(new RestTemplatePing(wrappedFactory))
-                .buildFixedServerListLoadBalancer(asList(new Server("localhost:8080"), new Server("localhost:8070")));
+        ConfigurationManager.loadPropertiesFromResources("ribbon.properties");
+        BaseLoadBalancer loadBalancer = (BaseLoadBalancer) ClientFactory.getNamedLoadBalancer("upstreamServerClient");
+        loadBalancer.setPing(new RestTemplatePing(wrappedFactory));
+
+//        ILoadBalancer loadBalancer  = LoadBalancerBuilder.newBuilder()
+//                .withPing(new RestTemplatePing(wrappedFactory))
+//                .buildFixedServerListLoadBalancer(asList(new Server("localhost:8080"), new Server("localhost:8070")));
 
         restTemplate = new RestTemplate(new SimpleRibbonClientHttpRequestFactoryWrapper(wrappedFactory, loadBalancer));
     }
@@ -54,11 +59,11 @@ public class Main {
         return exchange.getBody();
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         new Main().run();
     }
 
-    private class RestTemplatePing implements IPing {
+    public static class RestTemplatePing implements IPing {
         private final String pingPath = "/message";
 
         private final RestTemplate restTemplate;
